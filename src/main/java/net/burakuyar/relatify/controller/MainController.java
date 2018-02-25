8b0048a3dc7490bcdce624477ae55951fdc6a62c
Burak Uyar <burakuyar25@gmail.com>
2018-02-25 05:05:46 +0300

    MainController class has added. File uploading added.

---
+++ src/main/java/net/burakuyar/relatify/controller/MainController.java
@@ -0,0 +1,38 @@
+package net.burakuyar.relatify.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+/**
+ * Created by burak on 2/25/2018.
+ */
+@Controller
+public class MainController {
+    @GetMapping("/")
+    String index() {
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    String upload(@RequestParam("email") String email, @RequestParam("document") MultipartFile file, RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()){
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:/";
+        }
+        try {
+            InputStream pdfStream = new ByteArrayInputStream(file.getBytes());
+        }catch (Exception exception){
+            exception.printStackTrace();
+            redirectAttributes.addFlashAttribute("message", "Broken file!");
+            return "redirect:/";
+        }
+        return "index";
+    }
+}
