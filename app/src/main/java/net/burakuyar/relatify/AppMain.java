a861d76b76a1572919cbe8c4c02a56d38e03351f
Burak Uyar <burakuyar25@gmail.com>
2018-02-25 17:07:25 +0300

    Configuration updated.

--- app/src/main/java/net/burakuyar/relatify/AppMain.java
+++ app/src/main/java/net/burakuyar/relatify/AppMain.java
@@ -2,8 +2,11 @@ package net.burakuyar.relatify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScans(value = { @ComponentScan("net.burakuyar.relatify.dao"), @ComponentScan("net.burakuyar.relatify.service") })
 public class AppMain{
     public static void main(String args[]){
         SpringApplication.run(AppMain.class, args);
---
+++ app/src/main/java/net/burakuyar/relatify/config/DataConfig.java
@@ -0,0 +1,26 @@
+package net.burakuyar.relatify.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by burak on 2/25/2018.
+ */
+@Configuration
+public class DataConfig {
+    @Bean(destroyMethod = "close")
+    DataSource dataSource(Environment env) {
+        HikariConfig dataSourceConfig = new HikariConfig();
+        dataSourceConfig.setDriverClassName(env.getRequiredProperty("spring.datasource.driverClassName"));
+        dataSourceConfig.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+        dataSourceConfig.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        dataSourceConfig.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        return new HikariDataSource(dataSourceConfig);
+    }
+
+}
