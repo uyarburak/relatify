c952e0f02b220ead0f23ff31b56f8582cc479ef0
Burak Uyar <burakuyar25@gmail.com>
2018-02-25 22:57:57 +0300

    db has changed (users can have multiple emails)

--- app/src/main/resources/sql/db_create.sql
+++ app/src/main/resources/sql/db_create.sql
@@ -1,8 +1,7 @@
-DROP TABLE IF EXIST person;
 CREATE TABLE person (
   hash_id bigint,
   gender char(1),
-  name varchar(127),
+name varchar(127),
   surname varchar(127),
   father_name varchar(127),
   mother_name varchar(127),
@@ -11,19 +10,22 @@ CREATE TABLE person (
   serie_no varchar(127),
   marital_status varchar(127),
   live_status varchar(127),
-  PRIMARY KEY(hash_id)
+PRIMARY KEY(hash_id)
 );
-DROP TABLE IF EXIST owner;
 CREATE TABLE owner (
-  hash_id BIGINT REFERENCES person(hash_id),
-  mail VARCHAR(127),
+  user_id BIGINT REFERENCES person(hash_id),
   pdf_json TEXT,
-  PRIMARY KEY (hash_id)
+PRIMARY KEY (user_id)
 );
-DROP TABLE IF EXIST user_relative;
 CREATE TABLE user_relative(
-  user_id BIGINT REFERENCES owner(hash_id),
+  user_id BIGINT REFERENCES owner(user_id),
   relative_id BIGINT REFERENCES person(hash_id),
   relation VARCHAR(255),
-  PRIMARY KEY (user_id, relative_id)
+PRIMARY KEY (user_id, relative_id)
+);
+CREATE TABLE user_email(
+  user_id BIGINT REFERENCES owner(user_id),
+  email VARCHAR(255),
+  last_send_date TIMESTAMP DEFAULT NOW(),
+PRIMARY KEY (user_id, email)
 );
